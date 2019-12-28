182654ce025dab55e2fb6f91daf7c659627ea19e
Conti Matteo <contimatteo1505@gmail.com>
2019-12-28 15:29:45 +0100

    first commit

---
+++ Briscola-Multiplayer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Briscola-Multiplayer
+//
+//  Created by Matteo Conti on 28/12/2019.
+//  Copyright © 2019 Matteo Conti. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Briscola-MultiplayerTests/Briscola_MultiplayerTests.swift
@@ -0,0 +1,34 @@
+//
+//  Briscola_MultiplayerTests.swift
+//  Briscola-MultiplayerTests
+//
+//  Created by Matteo Conti on 28/12/2019.
+//  Copyright © 2019 Matteo Conti. All rights reserved.
+//
+
+import XCTest
+@testable import Briscola_Multiplayer
+
+class Briscola_MultiplayerTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
