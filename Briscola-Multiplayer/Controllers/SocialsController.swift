dabbdba3aaf05542964f24e586eaf8db47af57c6
Conti Matteo <contimatteo1505@gmail.com>
2020-01-16 19:52:00 +0100

    facebook login manager setup done

--- Briscola-Multiplayer/Controllers/SocialsController.swift
+++ Briscola-Multiplayer/Controllers/SocialsController.swift
@@ -8,5 +8,119 @@
 
 import Foundation
 import UIKit
+import FBSDKLoginKit
 
-class SocialsController: UIViewController {}
+
+class SocialsController: UIViewController {
+    
+    
+    @IBOutlet weak var FBCustomLoginButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    @IBAction func performFBLogin(_ sender: Any) {
+        let loginManager = LoginManager();
+        let permissions: Array<String> = ["public_profile", "user_friends"];
+        let from = self;
+        
+        loginManager.logIn(permissions: permissions, from: from) { (result, error) in
+            self.loginManagerDidComplete(result, errors: error);
+        }
+    }
+    
+    @IBAction private func performFBLogout() {
+        let loginManager = LoginManager()
+        loginManager.logOut()
+        print("//// FB logout done!");
+    }
+    
+    // func loginManagerDidComplete(_ result: LoginManagerLoginResultBlock) {
+    func loginManagerDidComplete(_ result: LoginManagerLoginResult?, errors: Error?) {
+        if errors != nil {
+            // Process error
+            print("//// FB Login failed with error \(errors!)");
+            
+            return;
+        }
+        
+        guard let loginResult = result else {
+            print("//// FB Login: something went wrong.");
+            return;
+        }
+        
+        // LOGIN EXECUTED
+            
+        print("//// FB login result \(result!)");
+        
+        // IS CANCELED ?
+        if (loginResult.isCancelled) {
+            print("//// FB User cancelled login.");
+            return
+        }
+        
+        // LOGIN COMPLETED SUCCESSFULLY
+        // If you ask for multiple permissions at once, you
+        // should check if specific permissions missing
+        if loginResult.grantedPermissions.contains("email")
+        {
+            // Do work
+        }
+    }
+    
+    
+    
+    //    func fbLoginInitiate() {
+    //        let loginManager = LoginManager()
+    //        loginManager.logInWithReadPermissions(["public_profile", "email"], handler: {(result:LoginManagerLoginResult!, error:NSError!) -> Void in
+    //            if (error != nil) {
+    //                // Process error
+    //                self.removeFbData()
+    //            } else if result.isCancelled {
+    //                // User Cancellation
+    //                self.removeFbData()
+    //            } else {
+    //                //Success
+    //                if result.grantedPermissions.contains("email") && result.grantedPermissions.contains("public_profile") {
+    //                    //Do work
+    //                    self.fetchFacebookProfile()
+    //                } else {
+    //                    //Handle error
+    //                }
+    //            }
+    //        })
+    //    }
+    //
+    //    func removeFbData() {
+    //        //Remove FB Data
+    //        let fbManager = LoginManager()
+    //        fbManager.logOut()
+    //        // AccessToken.setCurrentAccessToken(nil)
+    //        AccessToken.current = nil
+    //    }
+    //
+    //    func fetchFacebookProfile()
+    //    {
+    //        if AccessToken.current != nil {
+    //            let graphRequest : GraphRequest = GraphRequest(graphPath: "me", parameters: [:])
+    //            graphRequest.start(completionHandler: { (connection, result, error) -> Void in
+    //
+    //                if ((error) != nil) {
+    //                    //Handle error
+    //                } else {
+    //                    //Handle Profile Photo URL String
+    //                    guard let user = result as? [String: Any] else { return }
+    //
+    //                    let userId =  user["id"] as! String
+    //                    let profilePictureUrl = "https://graph.facebook.com/\(userId)/picture?type=large"
+    //
+    //                    let accessToken = AccessToken.current!.tokenString
+    //                    let fbUser = ["accessToken": accessToken, "user": result]
+    //                }
+    //            })
+    //        }
+    //    }
+    
+}
---
+++ Podfile
@@ -0,0 +1,22 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'Briscola-Multiplayer' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for Briscola-Multiplayer
+  pod 'FBSDKCoreKit'
+  pod 'FBSDKLoginKit'
+  pod 'FBSDKShareKit'
+
+  target 'Briscola-MultiplayerTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'Briscola-MultiplayerUITests' do
+    # Pods for testing
+  end
+
+end
