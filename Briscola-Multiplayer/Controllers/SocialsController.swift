7947037aba2f449b8dfd14862991bb81017a2388
Conti Matteo <contimatteo1505@gmail.com>
2020-01-17 16:57:56 +0100

    created facebook manager library

--- Briscola-Multiplayer/Controllers/SocialsController.swift
+++ Briscola-Multiplayer/Controllers/SocialsController.swift
@@ -13,32 +13,32 @@ import FBSDKLoginKit
 
 class SocialsController: UIViewController {
     
+    //
+    // MARK:
+    
+    var facebookManager: FacebookManager?;
     
     @IBOutlet weak var FBCustomLoginButton: UIButton!
+    
+    //
+    // MARK:
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        self.facebookManager = FacebookManager(permissions: ["public_profile", "user_friends"]);
     }
     
     @IBAction func performFBLogin(_ sender: Any) {
-        let loginManager = LoginManager();
-        let permissions: Array<String> = ["public_profile", "user_friends"];
-        let from = self;
-        
-        loginManager.logIn(permissions: permissions, from: from) { (result, error) in
-            self.loginManagerDidComplete(result, errors: error);
-        }
+        facebookManager!.login(from: self, didCompleteHandler: self.loginDidComplete);
     }
     
     @IBAction private func performFBLogout() {
-        let loginManager = LoginManager()
-        loginManager.logOut()
+        facebookManager!.logout();
         print("//// FB logout done!");
     }
     
-    // func loginManagerDidComplete(_ result: LoginManagerLoginResultBlock) {
-    func loginManagerDidComplete(_ result: LoginManagerLoginResult?, errors: Error?) {
+    func loginDidComplete(_ result: LoginManagerLoginResult?, errors: Error?) {
         if errors != nil {
             // Process error
             print("//// FB Login failed with error \(errors!)");
@@ -69,58 +69,5 @@ class SocialsController: UIViewController {
             // Do work
         }
     }
-    
-    
-    
-    //    func fbLoginInitiate() {
-    //        let loginManager = LoginManager()
-    //        loginManager.logInWithReadPermissions(["public_profile", "email"], handler: {(result:LoginManagerLoginResult!, error:NSError!) -> Void in
-    //            if (error != nil) {
-    //                // Process error
-    //                self.removeFbData()
-    //            } else if result.isCancelled {
-    //                // User Cancellation
-    //                self.removeFbData()
-    //            } else {
-    //                //Success
-    //                if result.grantedPermissions.contains("email") && result.grantedPermissions.contains("public_profile") {
-    //                    //Do work
-    //                    self.fetchFacebookProfile()
-    //                } else {
-    //                    //Handle error
-    //                }
-    //            }
-    //        })
-    //    }
-    //
-    //    func removeFbData() {
-    //        //Remove FB Data
-    //        let fbManager = LoginManager()
-    //        fbManager.logOut()
-    //        // AccessToken.setCurrentAccessToken(nil)
-    //        AccessToken.current = nil
-    //    }
-    //
-    //    func fetchFacebookProfile()
-    //    {
-    //        if AccessToken.current != nil {
-    //            let graphRequest : GraphRequest = GraphRequest(graphPath: "me", parameters: [:])
-    //            graphRequest.start(completionHandler: { (connection, result, error) -> Void in
-    //
-    //                if ((error) != nil) {
-    //                    //Handle error
-    //                } else {
-    //                    //Handle Profile Photo URL String
-    //                    guard let user = result as? [String: Any] else { return }
-    //
-    //                    let userId =  user["id"] as! String
-    //                    let profilePictureUrl = "https://graph.facebook.com/\(userId)/picture?type=large"
-    //
-    //                    let accessToken = AccessToken.current!.tokenString
-    //                    let fbUser = ["accessToken": accessToken, "user": result]
-    //                }
-    //            })
-    //        }
-    //    }
     
 }
---
+++ Briscola-Multiplayer/Libraries/FacebookManager.swift
@@ -0,0 +1,107 @@
+//
+//  FacebookManager.swift
+//  Briscola-Multiplayer
+//
+//  Created by Matteo Conti on 17/01/2020.
+//  Copyright © 2020 Matteo Conti. All rights reserved.
+//
+
+import Foundation
+import FBSDKLoginKit
+
+
+class FacebookManager {
+    
+    //
+    // MARK:
+    
+    private var permissions: [String];
+    private var loginManager: LoginManager;
+    private var currentUserProfile : [String: Any]?;
+    
+    //
+    // MARK:
+    
+    init(permissions: [String]) {
+        self.permissions = permissions;
+        self.loginManager = LoginManager();
+        self.currentUserProfile = nil;
+        
+        /// TODO: auto login user if token is already setted.
+    }
+    
+    //
+    // MARK
+    
+    public func login(from: UIViewController, didCompleteHandler: LoginManagerLoginResultBlock!) {
+        // self.loginManager.logIn(permissions: self.permissions, from: from, handler: handler);
+        
+        self.loginManager.logIn(permissions: self.permissions, from: from) { (result, error) in
+            self.loginDidComplete(result, error: error, didComplete: didCompleteHandler);
+        }
+    }
+    
+    public func logout() {
+        self.loginManager.logOut()
+        self.removeCurrentUserData()
+    }
+    
+    func fetchUserProfile(callback: @escaping (_ user: [String: Any]?) -> ()) {
+        if AccessToken.current == nil { return; }
+        
+        let graphRequest : GraphRequest = GraphRequest(graphPath: "me", parameters: [:])
+        graphRequest.start(completionHandler: { (connection, result, error) -> Void in
+            if error != nil {
+                // TODO: handle error
+                // ....
+                
+                return callback(nil);
+            }
+            
+            // Handle Profile Photo URL String
+            guard let user = result as? [String: Any] else { return }
+            self.currentUserProfile = user;
+            
+            callback(user);
+        });
+    }
+    
+    public func getUserProfileUrl() -> String? {
+        guard let userProfile = self.currentUserProfile else { return nil; }
+        let userId = userProfile["id"] as! String;
+        
+        return "https://graph.facebook.com/\(userId)/picture?type=large";
+    }
+    
+    //
+    // MARK
+    
+    private func loginDidComplete(_ result: LoginManagerLoginResult?, error: Error?, didComplete: LoginManagerLoginResultBlock!) {
+        // error
+        if error != nil {
+            self.removeCurrentUserData()
+        }
+        
+        // login canceled
+        if error == nil && result != nil {
+            if result!.isCancelled {
+                self.removeCurrentUserData()
+            }
+        }
+        
+        // call parent callback
+        didComplete(result, error);
+    }
+    
+    private func removeCurrentUserData() {
+        self.loginManager.logOut()
+        
+        // TODO: check this...
+        // AccessToken.setCurrentAccessToken(nil)
+        AccessToken.current = nil
+        
+        // empty current user var
+        currentUserProfile = nil;
+    }
+    
+}
